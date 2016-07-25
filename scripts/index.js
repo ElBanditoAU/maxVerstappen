b70655f955a237782679529918c9e9a7a0fd44f5
Jonathan Wong <jwong.au@gmail.com>
2016-07-25 11:31:10 +1000

    Start

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $('.gallery').slick({
+  	dots: true,
+  });
+});
+
+$(document).ready(function(){
+  $('#compare').slick({
+  	dots: true,
+  	// draggable: false
+  });
+});
+
+console.log("dog")
+
